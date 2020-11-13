53decb0f47a2eb8ade36eeec12985970273ab0da
arnu515 <arnu5152@gmail.com>
2020-11-14 00:08:14 +0530

    part 4!

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import App from './App.svelte';
 const app = new App({
 	target: document.body,
 	props: {
-		strapiApiUrl: 'https://quickstagram-backend.herokuapp.com'
+		strapiApiUrl: 'https://localhost:1337'
 	}
 });
 
